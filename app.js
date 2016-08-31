c0f59e85dd097625688d77af7b337ac25e3fcd0f
lorenzoi <lorenzoiannuzzi@gmail.com>
2016-08-31 18:34:43 +0200

    Removed Token (Not Valid)

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var Discord = require('discord.js');
 
 var bot = new Discord.Client();
 
-bot.on("message", function(message))
+bot.on("message", function(message)
 {
     if(message.content === "Hi")
     (
-        bot.reply(message, "Hey, There");
+        bot.reply(message, "Hey, There")
     )
 });
 
-bot.loginWithToken("MjlwNTc1ODQ0Njg5NTEwNDAw.CqiS8w.2xMJ5hTS2McRP4w7CyWmedHpETU");
+bot.loginWithToken("");
